48b1b3f5af98ded4dc76c9a54d5bf9895a3963d1
Aman-Mishra-Exarrow <mishraaman645@gmail.com>
2026-01-20 15:12:09 +0530

    deleted s3 file

--- terra-config/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "aman-terra-state-bucket"  # Change if the name already exists. 
-    key            = "eks/terraform.tfstate"       
-    region         = "us-east-1"                   
-    encrypt        = true
-  }
-}
