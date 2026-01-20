412b4b9469eb5cc4568a208130eb55498e767e81
Aman-Mishra-Exarrow <mishraaman645@gmail.com>
2026-01-20 17:25:15 +0530

    Migrate Terraform state to S3 backend and stabilize EKS

--- terra-config/eks.tf
+++ terra-config/eks.tf
@@ -1,14 +1,22 @@
-
-# Create the EKS Cluster
 resource "aws_eks_cluster" "eks_cluster" {
-  name = "Three-tier-cloud"
-
-  role_arn = aws_iam_role.cluster_role.arn
+  name     = "Three-tier-cloud"
+  role_arn = "arn:aws:iam::642825399412:role/eks-role-cluster"
 
   vpc_config {
     subnet_ids = data.aws_subnets.public.ids
   }
-  depends_on = [
-    aws_iam_role_policy_attachment.cluster_AmazonEKSClusterPolicy,
-  ]
+
+  bootstrap_self_managed_addons = false
+
+  lifecycle {
+    ignore_changes = [
+      access_config,
+      kubernetes_network_config,
+      upgrade_policy,
+      enabled_cluster_log_types,
+      tags,
+      vpc_config[0].security_group_ids,
+      vpc_config[0].public_access_cidrs,
+    ]
+  }
 }
--- terra-config/node_group.tf
+++ terra-config/node_group.tf
@@ -1,46 +1,18 @@
 resource "aws_eks_node_group" "example" {
   cluster_name    = aws_eks_cluster.eks_cluster.name
   node_group_name = "EKS_NODE_GROUP"
-  node_role_arn   = aws_iam_role.nodegroup_role.arn
-  subnet_ids      = data.aws_subnets.public.ids
+
+  node_role_arn = "arn:aws:iam::642825399412:role/eks-node-group-example"
+  subnet_ids     = data.aws_subnets.public.ids
+
+  launch_template {
+    name    = "Three-tier-cloud-node-template"
+    version = "$Latest"
+  }
 
   scaling_config {
     desired_size = 1
     max_size     = 1
     min_size     = 1
-  }
-
-  update_config {
-    max_unavailable = 1
-  }
-
-  launch_template {
-    id      = aws_launch_template.eks_node_launch_template.id
-    version = "$Latest"
-  }
-
-  depends_on = [
-    aws_iam_role_policy_attachment.example-AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.example-AmazonEKS_CNI_Policy,
-    aws_iam_role_policy_attachment.example-AmazonEC2ContainerRegistryReadOnly,
-    aws_launch_template.eks_node_launch_template,
-  ]
-}
-
-resource "aws_launch_template" "eks_node_launch_template" {
-  name = "${aws_eks_cluster.eks_cluster.name}-node-template" 
-
-  instance_type = "c7i-flex.large"
-
-  metadata_options {
-    http_endpoint             = "enabled"
-    http_tokens               = "required"
-    http_put_response_hop_limit = 2 
-  }
-  tags = {
-    Name = "${aws_eks_cluster.eks_cluster.name}-node-template"
-  }
-  lifecycle {
-    create_before_destroy = true 
   }
 }
