848f7f87ad3cac908219c00dacf62265d0ce9404
Aman-Mishra-Exarrow <mishraaman645@gmail.com>
2026-01-22 11:37:38 +0530

    changing some details

--- terra-config/eks.tf
+++ terra-config/eks.tf
@@ -1,10 +1,10 @@
 resource "aws_eks_cluster" "eks_cluster" {
   name     = "Three-tier-cloud"
-  role_arn = data.aws_iam_role.eks_cluster_role.arn
+  role_arn = "arn:aws:iam::642825399412:role/eks-role-cluster"
 
   vpc_config {
-    subnet_ids              = data.aws_subnets.public.ids
-    endpoint_public_access  = true
-    endpoint_private_access = false
+    subnet_ids = data.aws_subnets.public.ids
   }
+
+  bootstrap_self_managed_addons = false
 }
