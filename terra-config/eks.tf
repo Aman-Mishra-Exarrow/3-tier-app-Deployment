47365a6b01896a7c2d5519544a120b1804570679
Aman-Mishra-Exarrow <mishraaman645@gmail.com>
2026-01-26 20:31:27 +0530

    addid k8s

--- terra-config/eks.tf
+++ terra-config/eks.tf
@@ -3,6 +3,9 @@ resource "aws_eks_cluster" "eks" {
   role_arn = aws_iam_role.eks_cluster.arn
   version  = "1.34"
 
+access_config {
+  authentication_mode = "API_AND_CONFIG_MAP"
+}
    
   vpc_config {
     subnet_ids              = aws_subnet.private[*].id
@@ -13,4 +16,19 @@ resource "aws_eks_cluster" "eks" {
   depends_on = [
     aws_iam_role_policy_attachment.eks_cluster_policy
   ]
+}
+
+resource "aws_eks_access_entry" "admin" {
+  cluster_name  = aws_eks_cluster.eks.name
+  principal_arn = "arn:aws:iam::642825399412:user/test-user-501"
+}
+
+resource "aws_eks_access_policy_association" "admin_policy" {
+  cluster_name  = aws_eks_cluster.eks.name
+  principal_arn = "arn:aws:iam::642825399412:user/test-user-501"
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+
+  access_scope {
+    type = "cluster"
+  }
 }
