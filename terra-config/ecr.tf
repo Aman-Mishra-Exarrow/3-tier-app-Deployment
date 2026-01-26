abfadf3fc984360202ae79843548b0ec0337b730
Aman-Mishra-Exarrow <mishraaman645@gmail.com>
2026-01-26 18:28:18 +0530

    using private ecr

--- terra-config/ecr.tf
+++ terra-config/ecr.tf
@@ -1,8 +1,9 @@
-resource "aws_ecrpublic_repository" "frontend" {
-  repository_name  = "three-tier-frontend"
+resource "aws_ecr_repository" "frontend" {
+  name         = "three-tier-frontend"
+  force_delete = true
 }
 
-resource "aws_ecrpublic_repository" "backend" {
-  repository_name  = "three-tier-backend"
-  
+resource "aws_ecr_repository" "backend" {
+  name         = "three-tier-backend"
+  force_delete = true
 }
