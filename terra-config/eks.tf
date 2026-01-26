e276caa51387b6411e8507bdfa35eb838697c095
Aman-Mishra-Exarrow <mishraaman645@gmail.com>
2026-01-26 20:01:36 +0530

    adding user in k8s config

--- terra-config/eks.tf
+++ terra-config/eks.tf
@@ -3,6 +3,7 @@ resource "aws_eks_cluster" "eks" {
   role_arn = aws_iam_role.eks_cluster.arn
   version  = "1.34"
 
+   
   vpc_config {
     subnet_ids              = aws_subnet.private[*].id
     endpoint_public_access  = true
@@ -12,4 +13,11 @@ resource "aws_eks_cluster" "eks" {
   depends_on = [
     aws_iam_role_policy_attachment.eks_cluster_policy
   ]
+}
+
+resource "aws_eks_access_entry" "admin_user" {
+  cluster_name      = aws_eks_cluster.eks.name
+  principal_arn     = "arn:aws:iam::642825399412:user/test-user-501"
+  kubernetes_groups = ["system:masters"]
+  type             = "STANDARD"
 }
