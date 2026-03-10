9b44a12eb1da175379c72fda06a9a18a1bc41e9f
Aman-Mishra-Exarrow <72553456+Aman-Mishra-Exarrow@users.noreply.github.com>
2026-03-10 15:58:56 +0530

    Update S3 bucket name for Terraform backend

--- terra-config/backend.tf
+++ terra-config/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "aman-eks-tf-state"
+    bucket         = "aman-eks-tf-state-1"
     key            = "3-tier/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-lock"
